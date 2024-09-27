c3ae913ed65051b3474c940465edfb9af14962f6
alexzaznobin <alex.zaznobin@gmail.com>
2024-09-28 00:50:26 +0300

    port in client

--- clickhouse_client.py
+++ clickhouse_client.py
@@ -2,8 +2,8 @@ from clickhouse_driver import Client
 
 
 class ClickhouseClient :
-    def __init__ (self, host, user, password) :
-        self.client = Client(host=host, user=user, password=password)
+    def __init__ (self, host='clickhouse', port=9000, user='default', password='') :
+        self.client = Client(host=host, port=port, user=user, password=password)
 
     def execute (self, query, params=None) :
         return self.client.execute(query, params)
