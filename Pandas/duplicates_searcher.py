f04dc2688827d10fc7a4ad164fa7cadf6d795044
Kidein <96892710+Kidein@users.noreply.github.com>
2024-09-28 16:39:06 +0300

    Update duplicates_searcher.py
    ---

--- Pandas/duplicates_searcher.py
+++ Pandas/duplicates_searcher.py
@@ -1,59 +1,60 @@
-from Levenshtein import distance as levenshtein_distance
 import pandas as pd
 
+
 class DuplicatesSearcher:
-    def __init__(self, df, threshold=3):
+    def __init__(self, df, exclude_columns=None, uid_column='uid'):
         self.df = df
-        self.threshold = threshold
+        # Колонка, которая служит уникальным идентификатором для строк
+        self.uid_column = uid_column
+        # Исключённые колонки
+        self.exclude_columns = exclude_columns if exclude_columns else []
 
-    # in table comparison
-    def find_duplicates(self, column):
-        duplicates = []
-        for i in range(len(self.df)):
-            for j in range(i + 1, len(self.df)):
-                dist = levenshtein_distance(str(self.df.iloc[i][column]), str(self.df.iloc[j][column]))
-                if dist <= self.threshold:
-                    duplicates.append((i, j, dist))
+    # Внутреннее сравнение дубликатов по каждой колонке, кроме указанных в exclude_columns
+    def find_duplicates(self):
+        duplicates = {}
+        for column in self.df.columns:
+            if column not in self.exclude_columns and column != self.uid_column:
+                # Находим дубликаты по текущей колонке
+                dup_mask = self.df.duplicated(subset=[column], keep=False)
+                duplicates_in_column = self.df[dup_mask]
+                if not duplicates_in_column.empty:
+                    duplicates[column] = duplicates_in_column
         return duplicates
 
-    # comparison between tables
-    def find_duplicates_between(self, other_df, column):
-        duplicates = []
-        for i in range(len(self.df)):
-            for j in range(len(other_df)):
-                dist = levenshtein_distance(str(self.df.iloc[i][column]), str(other_df.iloc[j][column]))
-                if dist <= self.threshold:
-                    duplicates.append((i, j, dist))
-        return duplicates
+    # Сравнение дубликатов между двумя таблицами по каждой колонке, кроме указанных в exclude_columns
+    def find_duplicates_between(self, other_df):
+        results = []
+        # Находим общие колонки для сравнения, исключая указанные
+        common_columns = self.df.columns.intersection(other_df.columns).difference(self.exclude_columns)
+        for column in common_columns:
+            if column != self.uid_column:
+                # Объединяем таблицы по текущей колонке, сохраняем уникальные ID строк
+                merged_df = pd.merge(
+                    self.df[[self.uid_column, column]],
+                    other_df[[self.uid_column, column]],
+                    on=column,
+                    how='inner',
+                    suffixes=('_self', '_other')
+                )
+                if not merged_df.empty:
+                    merged_df['matching_column'] = column
+                    results.append(
+                        merged_df[[f'{self.uid_column}_self', f'{self.uid_column}_other', 'matching_column']])
 
-    # table of duplicates within one table
-    def build_duplicates_table(self, columns):
-        duplicate_rows = set()
-        for column in columns:
-            duplicates = self.find_duplicates(column)
-            for i, j, _ in duplicates:
-                duplicate_rows.add(i)
-                duplicate_rows.add(j)
+        # Объединяем все результаты в один DataFrame
+        if results:
+            final_result = pd.concat(results, ignore_index=True)
+        else:
+            final_result = pd.DataFrame(
+                columns=[f'{self.uid_column}_self', f'{self.uid_column}_other', 'matching_column'])
 
-        return self.df.loc[list(duplicate_rows)]
+        return final_result
 
-    # table of duplicates between two tables
-    def build_duplicates_table_between(self, other_df, columns):
-        duplicate_rows_self = set()
-        duplicate_rows_other = set()
+    # Таблица дубликатов между двумя таблицами
+    def build_duplicates_table_between(self, other_df):
+        duplicates_between = self.find_duplicates_between(other_df)
+        return duplicates_between
 
-        for column in columns:
-            duplicates = self.find_duplicates_between(other_df, column)
-            for i, j, _ in duplicates:
-                duplicate_rows_self.add(i)
-                duplicate_rows_other.add(j)
-
-        return (self.df.loc[list(duplicate_rows_self)], other_df.loc[list(duplicate_rows_other)])
-
-    def display_duplicates(self, columns):
-        duplicates_table = self.build_duplicates_table(columns)
-        return duplicates_table
-
-    def display_duplicates_between(self, other_df, columns):
-        df_duplicates, other_df_duplicates = self.build_duplicates_table_between(other_df, columns)
-        return df_duplicates, other_df_duplicates
+    def display_duplicates_between(self, other_df):
+        duplicates_between_table = self.build_duplicates_table_between(other_df)
+        return duplicates_between_table
